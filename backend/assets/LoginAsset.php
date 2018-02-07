f89f532e7389b067ff78ca9a4d08e5ae84ad9aa5
farhanz95 <farhanz95@gmail.com>
2018-02-07 19:14:05 +0800

    .

---
+++ backend/assets/LoginAsset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class LoginAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/loginsite.css',
+        'theme/assets/skin/default_skin/css/logintheme.css',
+        'theme/assets/admin-tools/admin-forms/css/admin-forms.css',
+    ];
+    public $js = [
+        // 'theme/vendor/jquery/jquery-1.11.1.min.js',
+        // 'theme/vendor/jquery/jquery_ui/jquery-ui.min.js',
+        'theme/assets/js/utility/utility.js',
+        'theme/assets/js/demo/demo.js',
+        'theme/assets/js/main.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -10,26 +10,102 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+  
+    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-    <p>Please fill out the following fields to login:</p>
+    <div class="tab-content mw900 center-block center-children">
+    <!-- Login 2 -->
+      <div class="admin-form theme-primary tab-pane active" id="login2" role="tabpanel">
+        <div class="panel panel-primary heading-border">
+          <div class="panel-heading">
+            <span class="panel-title">
+              <i class="fa fa-sign-in"></i>Login</span>
+          </div>
+          <!-- end .form-header section -->
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+          <form method="post" action="/" id="form-login2">
+            <div class="panel-body p25 pt10">
+              <div class="section row">
+                <div class="col-md-6">
+                  <div class="section-divider mv40">
+                    <span>Sign in With</span>
+                  </div>
+                  <!-- .tagline -->
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                  <div class="section">
+                    <a href="#" class="button btn-social facebook span-left btn-block">
+                      <span>
+                        <i class="fa fa-facebook"></i>
+                      </span>Facebook</a>
+                    <a href="#" class="button btn-social twitter span-left btn-block">
+                      <span>
+                        <i class="fa fa-twitter"></i>
+                      </span>Twitter</a>
+                    <a href="#" class="button btn-social googleplus span-left btn-block">
+                      <span>
+                        <i class="fa fa-google-plus"></i>
+                      </span>Google+</a>
+                  </div>
+                  <!-- end section -->
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
+                <!-- end .colm section -->
 
-            <?php ActiveForm::end(); ?>
+                <div class="col-md-6">
+                  <div class="section-divider mv40">
+                    <span>OR Login</span>
+                  </div>
+                  <!-- .tagline -->
+
+                  <?= $form->field($model, 'username',['template'=>'
+                  <label for="username" class="field prepend-icon">
+                    {input}{hint}{error}
+                    <label for="username" class="field-icon">
+                      <i class="fa fa-user"></i>
+                  </label>
+                  '])->textInput(['autofocus' => false,'placeholder'=>'Enter username'])->label(false) ?>
+
+                  <!-- end section -->
+
+                  <?= $form->field($model, 'password',['template'=>'
+                  <label for="password" class="field prepend-icon">
+                    {input}{hint}{error}
+                    <label for="password" class="field-icon">
+                      <i class="fa fa-lock"></i>
+                  </label>
+                  '])->passwordInput(['autofocus' => false,'placeholder'=>'Enter password'])->label(false) ?>
+
+                  <!-- end section -->
+
+                  <div class="section">
+                    <label class="switch block">
+                      <input type="checkbox" name="remember2" id="remember2" checked>
+                      <label for="remember2" data-on="YES" data-off="NO"></label>
+                      <span>Remember me</span>
+                    </label>
+                  </div>
+
+                  <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+                  <!-- end section -->
+
+                </div>
+                <!-- end .colm section -->
+
+              </div>
+              <!-- end .section row section -->
+
+            </div>
+            <!-- end .form-body section -->
+            <div class="panel-footer">
+              <button type="submit" class="button btn-primary">Sign in</button>
+            </div>
+            <!-- end .form-footer section -->
+          </form>
         </div>
+        <!-- end .admin-form section -->
+      </div>
+      <!-- end: .admin-form --> 
     </div>
-</div>
+
+    <?php ActiveForm::end(); ?>
